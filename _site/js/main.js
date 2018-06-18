3f034498e6b2b647f3b9574ad20c6087699417ca
Anna <anna.franklyn@gmail.com>
2018-06-18 16:15:53 +0800

    show me javascript added

---
+++ _site/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  //code goes in here */
+
+  // JavaScript
+  window.sr = ScrollReveal();
+  sr.reveal('.show-me', { duration: 2000 });
+  // sr.reveal('.bar');
+
+});
